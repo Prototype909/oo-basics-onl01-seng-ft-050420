dded112a9897a211b969de14019873bf9a8081f7
Steve Anderson <prototype909@msn.com>
2020-05-13 18:30:46 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,6 +3,10 @@ class Shoe
 
   attr_reader :brand
   attr_accessor :color, :size, :material, :condition
+  
+  def initialize(brand)
+    @brand = brand
+  end
 
   def initialize(brand)
     @brand = brand
